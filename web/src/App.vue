14f52b21e2bd05e4cdc31a23565dfd1fe6edcf7a
hanako mumei <81144685+2501babe@users.noreply.github.com>
2021-06-02 05:47:50 +0000

    init frontend bullshit

---
+++ web/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <Main msg="navy"/>
+</template>
+
+<script>
+import Main from "./components/Main.vue";
+//import * as w3 from "../node_modules/@solana/web3.js";
+
+export default {
+  name: 'App',
+  components: {
+    Main
+  }
+}
+</script>
+
+<style>
+#app {
+    align: center;
+    text-align: center;
+}
+</style>
---
+++ web/src/components/Main.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+    <p>i love to blog</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Main',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
